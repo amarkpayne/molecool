8a70f36d5f84fb399fd6ef1bc6d6b04c51e24987
Mark <ampayne@mit.edu>
2021-01-12 13:42:11 -0500

    Add unit test for calculate distance

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -7,6 +7,21 @@ import molecool
 import pytest
 import sys
 
+import numpy as np
+
+
+def test_calculate_distance():
+    """Test that the calculate distance function calculates what we expect."""
+    r1 = np.array([1, 0, 0])
+    r2 = np.array([1, 3, 4])
+
+    expected_distance = 5
+
+    observed_distance = molecool.calculate_distance(r1, r2)
+
+    assert expected_distance == observed_distance
+
+
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
